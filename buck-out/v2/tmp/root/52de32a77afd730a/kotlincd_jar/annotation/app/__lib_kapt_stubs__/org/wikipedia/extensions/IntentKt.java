6d246627704998d2a0f1ce272b4db01f135fabab
Mahdi Ahmed <b41z33d@meta.com>
2025-07-31 16:53:47 +0100

    wip: resolving unresolved reference to databinding

--- buck-out/v2/tmp/root/52de32a77afd730a/kotlincd_jar/annotation/app/__lib_kapt_stubs__/org/wikipedia/extensions/IntentKt.java
+++
@@ -1,9 +0,0 @@
-package org.wikipedia.extensions;
-
-import android.content.Intent;
-import android.os.Parcelable;
-import androidx.core.content.IntentCompat;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0016\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a*\u0010\u0000\u001a\u0004\u0018\u0001H\u0001\"\n\b\u0000\u0010\u0001\u0018\u0001*\u00020\u0002*\u00020\u00032\b\u0010\u0004\u001a\u0004\u0018\u00010\u0005H\u0086\b\u00a2\u0006\u0002\u0010\u0006\u00a8\u0006\u0007"}, d2 = {"parcelableExtra", "T", "Landroid/os/Parcelable;", "Landroid/content/Intent;", "key", "", "(Landroid/content/Intent;Ljava/lang/String;)Landroid/os/Parcelable;", "app.lib"})
-public final class IntentKt {
-}
